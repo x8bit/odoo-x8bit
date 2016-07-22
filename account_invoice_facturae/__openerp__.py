7a8ba18ea00d2356c32810bb6ea6c846da031bdd
Juan Carlos del Valle <juan@x8bit.com>
2016-07-22 12:09:51 -0500

    starting with account invoice facturae module

---
+++ account_invoice_facturae/__openerp__.py
@@ -0,0 +1,22 @@
+{
+    'name': "Modulo que agrega la facturación electrónica a los invoices de odoo",
+    'version': '1.0',
+    'depends': [
+        'document',
+        'company_facturae_certs',
+        'l10n_mx_payment_method',
+        'l10n_mx_base_vat_split',
+        'l10n_mx_partner_address'
+    ],
+    'author': "X8BIT SA DE CV",
+    'website': 'www.x8bit.com',
+    'category': 'Accounting',
+    'description': """
+    Modulo que agrega la facturación electrónica a los invoices de odoo
+    """,
+    # data files always loaded at installation
+    'data': [
+        'security/ir.model.access.csv',
+        'views/invoice_facturae.xml',
+    ],
+}
