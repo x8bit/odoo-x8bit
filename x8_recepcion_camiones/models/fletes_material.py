52ce88516f76c421424f9729b04404f057df758f
Juan Carlos del Valle <juan@x8bit.com>
2016-08-17 16:43:53 -0500

    asginar zonas a materiales

--- x8_recepcion_camiones/models/fletes_material.py
+++ x8_recepcion_camiones/models/fletes_material.py
@@ -8,6 +8,11 @@ class FleteMaterial(models.Model):
 		if self.product_id:
 			self.precio = self.product_id.standard_price or 0.00
 
+	@api.onchange('project_id')
+	def _onchange_product(self):
+		if self.project_id:
+			self.zonas = []
+
 	name = fields.Char(string="Nombre del material", required=True)
 	product_id = fields.Many2one("product.template", string="Producto", required=True, domain=[('type', '=', 'product')])
 	banco = fields.Char(string="Banco", required=True)
@@ -16,4 +21,5 @@ class FleteMaterial(models.Model):
 	precio_flete = fields.Integer(string="Costo flete", required=True)
 	proveedor = fields.Many2one("res.partner", string="Proveedor")
 	project_id = fields.Many2one("project.project", string="Proyecto", required=True)
+	zonas = fields.Many2many("x8.flete.zona","zona_project_rel", "project_id", string="Zonas en las que esta disponible")
 
