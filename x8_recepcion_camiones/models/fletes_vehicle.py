d8601204069bd51a81a5f184179d41f307491fd4
Jackodi <lisi879@gmail.com>
2016-08-16 19:36:51 -0500

    fixes en fletes

--- x8_recepcion_camiones/models/fletes_vehicle.py
+++ x8_recepcion_camiones/models/fletes_vehicle.py
@@ -3,9 +3,9 @@ from openerp import models, fields
 class FleteVehiculo(models.Model):
     _name = 'x8.flete.vehicle'
 
-    name = fields.Char(string="Numero de placas", size = 8)
-    chofer = fields.Char(string="Nombre del chofer")
-    num_econ = fields.Integer(string="Numero economico")
+    name = fields.Char(string="Numero de placas", size = 8, required=True)
+    chofer = fields.Char(string="Nombre del chofer", required=True)
+    num_econ = fields.Integer(string="Numero economico", required=True)
     description = fields.Text(string="Descripcion del vehiculo")
     capacidad = fields.Float(string="Capacidad de carga en m3", required=True)
-    proyect_id = fields.Many2one("project.project", string="Proyecto")
+    proyect_id = fields.Many2one("project.project", string="Proyecto", required=True)
