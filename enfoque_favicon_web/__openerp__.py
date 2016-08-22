00636a53ab3feda76f77fada32b6b5348a9bcf51
Juan Carlos del Valle <juan@x8bit.com>
2016-08-22 18:22:33 -0500

    added favicon modules

---
+++ enfoque_favicon_web/__openerp__.py
@@ -0,0 +1,15 @@
+{
+    'name': "Favicon Enfoquearquitectonico",
+    'version': '1.0',
+    'depends': ['web'],
+    'author': "X8BIT SA DE CV",
+    'website': 'www.x8bit.com',
+    'category': 'Web',
+    'description': """
+    Agrega un favicon para Enfoquearquitectonico
+    """,
+    # data files always loaded at installation
+    'data': [
+        'views/website_templates.xml',
+    ],
+}
---
+++ x8_favicon_web/__openerp__.py
@@ -0,0 +1,15 @@
+{
+    'name': "Favicon X8BIT",
+    'version': '1.0',
+    'depends': ['web'],
+    'author': "X8BIT SA DE CV",
+    'website': 'www.x8bit.com',
+    'category': 'Web',
+    'description': """
+    Agrega un favicon para x8bit
+    """,
+    # data files always loaded at installation
+    'data': [
+        'views/website_templates.xml',
+    ],
+}
